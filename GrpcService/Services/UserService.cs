7c5402dfeaaf8b3c77343dfbc32e228a98477ed9
Evgeniy Shtykhlin <evgeniyshtykhlin@gmail.com>
2024-09-12 15:27:41 -0700

    Add user service.

---
+++ GrpcService/Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using GrpcService;
+using Grpc.Core;
+
+public class UserService : User.UserBase
+{
+    public override Task<UserReply> GetUserInfo(UserRequest request, ServerCallContext context)
+    {
+        var userReply = new UserReply
+        {
+            Name = "John Doe",
+            Age = 30,
+            Roles = { "admin", "editor" },
+            Address = new Address
+            {
+                Street = "123 Main St",
+                City = "New York",
+                Country = "USA"
+            },
+            Coordinates = new Coordinates
+            {
+                Latitude = 40.712776,
+                Longitude = -74.005974
+            }
+        };
+
+        return Task.FromResult(userReply);
+    }
+
+    public override Task<CreateUserReply> CreateUser(CreateUserRequest request, ServerCallContext context)
+    {
+        var newUserId = 1001;
+
+        return Task.FromResult(new CreateUserReply
+        {
+            UserId = newUserId,
+            Message = $"User {request.Name} created with ID {newUserId}"
+        });
+    }
+}
